6996fa4e98ba60c1bb5950974b74b3dbdfe847e5
ZoeFA <d22125460@mytudublin.ie>
2023-05-04 18:48:32 +0100

    shahida comments

--- java/src/c21333116/myHeart.java
+++ java/src/c21333116/myHeart.java
@@ -12,11 +12,11 @@ public class myHeart {
     //calling ourvisual class
     OurVisual v;
 
-	public myHeart(OurVisual v)
-	{
-			this.v = v;
-			
-	}
+    public myHeart(OurVisual v)
+    {
+            this.v = v;
+           
+    }
 
 
     float radius = 500;
@@ -33,13 +33,14 @@ public class myHeart {
 
     public void render()
     {
-        // v.applyColorMode();
-        
+       
         v.background(0);
+        //calculating amplitude
         v.calculateAverageAmplitude();
-        
+        //calculating frequency
         v.calculateFrequencyBands();
-            
+           
+            //calculating the colums to fill the screen
         int numColumns = v.width / 20;
         for (int i = 0; i < numColumns; i++) {
             v.stroke(0);
@@ -50,21 +51,27 @@ public class myHeart {
             }
             v.rect(i * 20, 0, 20, v.height); // draw the column
         }
-        
+       
 
         float amplitude = v.getSmoothedAmplitude();
+        //calculating scale of the heart baes on the heart scale
         float heartScale = 0.5f + amplitude * 2.0f;
+        //calculating size of the heart shape
         heartSize = 35 * heartScale;
-
+        // moves to the center of the screen
         v.translate(v.width/2, v.height/2);
-        
+       
 
         v.beginShape();
+        //loop that defines edges of the heart
         for (float a = 0; a < PApplet.TWO_PI; a += 0.01)
         {
+          //light pink color of heart
             v.fill(255, 192, 203);
             v.strokeWeight(3);
+            //formula that generates left side of the heart
             float x = 16 * PApplet.pow(PApplet.sin(a), 3);
+            ///calculates the y coordinate of the heart
             float y = -((13 * PApplet.cos(a)) - (5 * PApplet.cos(2*a)) - (2 * PApplet.cos(3*a)) - PApplet.cos(4*a));
             x *= heartSize;
             y *= heartSize;
--- java/src/example/twerking.java
+++ java/src/example/twerking.java
@@ -8,125 +8,82 @@ public class twerking {
     
     OurVisual v;
 
-	public twerking(OurVisual v)
-	{
-			this.v = v;
-			
-	}
+    public twerking(OurVisual v)
+    {
+            this.v = v;
+           
+    }
 
     public void render()
     {
         v.calculateAverageAmplitude();
-        
         v.calculateFrequencyBands();
        
-        
+       
         v.background(255,192,203);
-
         v.noFill();
-
         v.stroke(255,165,0);
-
         v.strokeWeight(10);
 
+          //calculating the x coordinate of the center of the screen
 
         int centerX = v.width / 2;
+          //calculating the y coordinate of the center of the screen
 
         int centerY = v.height / 2;
 
         float circleSize = 150;
-
         float spacing = 70;
 
-      
-
+     
+        // loop that draws circles till it reaches the width of the page
         while (circleSize < v.width) {//background circles/ellipses
 
             int x = centerX;
-
             int y = centerY;
-
        
-
+            //aspect ratio to make the ellipse look more like a circle
             float aspectRatio = (float) (0.2 * (circleSize / 100.0)); // calculate aspect ratio based on circle size
 
             v.ellipse(centerX, centerY, circleSize, circleSize * aspectRatio); // use dynamic aspect ratio
 
-      
+     
 
             circleSize += 90;
-
+              // adds space between the pixels
             spacing += 80; // increase the distance between circles
 
         }
 
-
- 
-
         //boy
-
+          //calculating the amplitude
         v.calculateAverageAmplitude();
 
-            
+        //calculating the frequency bands
+        v.calculateFrequencyBands();
 
-            v.calculateFrequencyBands();
+        v.fill(255, 165, 0); // Orange fill color
+        v.ellipse(centerX, 150, 70, 70);
+        v.fill(255);
+        v.rect(centerX - 50, 235, 100, 90);
+        v.fill(255);
+        v.stroke(255, 165, 0);
+        v.strokeWeight(28);
 
-          
-            
+        v.fill(255);         
+        v.line(900, 450, 900, 550);
+        v.fill(255);
+        v.line(1000, 450, 1000, 550);
+     
+        //asscheecks     
+        // ellipseSize:calculates the distance of the ellipses based on the amplit
 
-        
-
-            v.fill(255, 165, 0); // Orange fill color
-
-            v.ellipse(centerX, 200, 70, 70);
-            
-
-           
-
-            v.fill(255);
-
-            v.rect(centerX - 50, 235, 100, 90);
-
-      
-
-            v.fill(255);
-
-            v.stroke(255, 165, 0);
-
-            v.strokeWeight(28);
-
-           
-
-            v.fill(255);
-            
-           
-            //legs
-            v.line(900, 550, 900, 700);
-
-            v.fill(255);
-
-            v.line(1000, 550, 1000, 700);
-            //line(379, 450, 379, 500);
-
-            
-
-            //line(434, 350, 434, 390);
-
-      
-            //asscheecks
-
-            float ellipseSize = 50 + v.getSmoothedAmplitude() * 200;
-
-            float ellipseOffset = 400 + v.getSmoothedAmplitude() * 200;
-
-            v.fill(255);
-
-            v.ellipse(centerX - 45, ellipseOffset, ellipseSize, ellipseSize);
-
-      
-
-            v.ellipse(centerX + 30, ellipseOffset, ellipseSize, ellipseSize);
-        
+        float ellipseSize = 50 + v.getSmoothedAmplitude() * 200;
+        float ellipseOffset = 400 + v.getSmoothedAmplitude() * 200;
+        v.fill(255);
+        v.ellipse(centerX - 45, ellipseOffset, ellipseSize, ellipseSize);
+        v.ellipse(centerX + 30, ellipseOffset, ellipseSize, ellipseSize);
+       
     }
 
 }
